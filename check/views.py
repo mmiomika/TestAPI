a09e012378d7a3b23eb36341ed5db1430b1eaf55
mmiomika <zhakiianova.mm@phystech.edu>
2018-10-23 21:17:28 +0300

    add gets

--- check/views.py
+++ check/views.py
@@ -58,6 +58,9 @@ class DataList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def get(self, request, format=None):
+        return Response("WORK WORK WORK", status=status.HTTP_200_OK)
+
 class DataList2(APIView):
     def post(self,request,format=None):
         #start_time = time.time()
@@ -67,8 +70,6 @@ class DataList2(APIView):
         local_timezone = tzlocal.get_localzone()
         time_format = datetime.fromtimestamp(unix_timestamp, local_timezone)
         data1['clickDate'] = time_format.isoformat()
-        #try:
-        #    if data1['']
         test = pd.DataFrame.from_dict(data1, orient='index').T
         lb = LabelEncoder()
         lb.classes_ = np.load(os.path.join(BASE_DIR, 'check/classes.npy'))
@@ -169,3 +170,6 @@ class DataList2(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         '''
         return Response(serializer.data, status = status.HTTP_200_OK)
+
+    def get(self, request, format=None):
+        return Response("WORK WORK WORK", status=status.HTTP_200_OK)
